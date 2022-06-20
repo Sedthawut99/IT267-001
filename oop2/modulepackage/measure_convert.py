743bcba2280e88ad869056f5c5cecefb19f55472
Sedthawut99 <644608020010@dpu.ac.th>
2022-06-20 15:15:08 +0700

    update

---
+++ oop2/modulepackage/measure_convert.py
@@ -0,0 +1,5 @@
+def cm_to_inch(measure:float):
+    return(measure / 2.54)
+
+def inch_to_cm(measure:float):
+    return(measure * 2.54)
---
+++ oop2/modulepackage/my_math_use2.py
@@ -0,0 +1,6 @@
+from my_math import square,circle_area
+
+if __name__ == '__main__':
+    square_number = square(5)
+    print(square_number)
+    print(circle_area(3))

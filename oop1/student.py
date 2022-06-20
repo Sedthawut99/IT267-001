dbce702d1e9fc8164353c9abbdf661aa4efff027
Sedthawut99 <644608020010@dpu.ac.th>
2022-06-20 09:18:58 +0700

    modify student and  pizza

--- oop1/student.py
+++ oop1/student.py
@@ -1,5 +1,8 @@
 class Student:
-    def __init__(self,id:str,name:str,major:str):
+    #major:str  = "IT"
+    #major = "IT"
+    #major: "IT" error
+    def __init__(self,id:str,name:str,major:str = "IT"):
         self.id = id
         self.name = name
         self.major = major
@@ -15,5 +18,9 @@ class Student:
 if __name__ == "__main__":
     jes = Student("111","Jesica","IT")
     john = Student("112","John","MKT")
+    amy = Student("113","Amy")
+
     jes.display_detail()
-    john.display_detail()    
+    john.display_detail() 
+    amy.display_detail()
+

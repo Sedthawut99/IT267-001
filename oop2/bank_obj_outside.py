725bb3d027f1bf779f3775e6102af53d763759bb
Sedthawut99 <644608020010@dpu.ac.th>
2022-06-21 11:01:35 +0700

    package bank

---
+++ oop2/bank_obj_outside.py
@@ -0,0 +1,23 @@
+#this file in outside bank package
+#call module
+#from customer import Customer
+#from account import Account
+#========================================
+#from bank import customer,account 
+#cus1 = customer.Customer()
+#cus1_acc = account.Account()
+#========================================
+from bank.customer import Customer
+from bank.account import Account
+
+cus1 = Customer()
+cus1.new_customer()
+
+cus1_acc = Account()
+cus1_acc.open_account(cus1.name,"Saving",'334-201-220-2',500)
+
+print("==== Open Bank Account Detail ====")
+print(cus1.cus_info())
+print(cus1_acc.display_balance())
+
+#print(cus1.cus_info())

f65470a2dc88450b57e73625073adcd29118fe09
Sedthawut99 <644608020010@dpu.ac.th>
2022-07-04 11:47:08 +0700

    vehicle

---
+++ oop2/abstraction/vehicle/vehicle_subclass.py
@@ -0,0 +1,38 @@
+from vehicle_abstract import Vehicle
+class Car(Vehicle):
+    def __init__(self, brand, speed) -> None:
+        #super().__init__(brand, speed)
+        Vehicle.__init__(self,brand,speed)
+        self.__year = 0
+        self.__maintanance = 0
+    @property #year
+    def year(self):
+        return self.__year
+    @year.setter
+    def year(self,value):
+        self.__year = value
+
+    @property
+    def maintanance(self):
+        return self.__maintanance
+    @maintanance.setter
+    def maintanance(self,value):
+        self.__maintanance = value
+
+    #implement abstact method
+    def show_detail(self):
+        return super().show_detail()
+        print('=== Car Detail ===')
+        print(f'{self.brand} with speed {self.speed} km/hr')
+        print(f'manufactered in {self.year} has')
+        print(f'{self.gear} gear and {self.seat} seats')
+
+    #implement Car method
+    def show_maintanance(self):
+        print('-- Car Maintanance --')
+        print(f'The lastest maintance in {self.maintanance}')
+class Truck(Vehicle):
+    pass
+
+class Motocycle(Vehicle):
+    pass
---
+++ oop2/area/shape.py
@@ -0,0 +1,22 @@
+class Shape:
+    def __init__(self,shape) -> None:
+        self.__shape = shape
+        self.__area = 0
+
+    @property
+    def shape(self):
+        return self.__shape
+    @shape.setter
+    def shape(self,value):
+        self.__shape = value
+
+    #area property
+    @property
+    def area(self):
+        return self.__area
+    @area.setter
+    def area(self,value):
+        self.__area = value
+        
+    def compute_area(self):
+        pass
